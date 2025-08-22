4c7056312bdb053ef6d663fb0b7914faf7e3575b
Hiendang7613 <73353492+hiendang7613@users.noreply.github.com>
2025-08-22 14:56:30 +0700

    Update engine.py

--- rfdetr/engine.py
+++ rfdetr/engine.py
@@ -300,6 +300,12 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, arg
 
         orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
         results = postprocessors["bbox"](outputs, orig_target_sizes)
+        if "segm" in postprocessors:
+            target_sizes = torch.stack([t["size"] for t in targets], dim=0)
+            results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
+        
+            
+
         res = {
             target["image_id"].item(): output
             for target, output in zip(targets, results)
